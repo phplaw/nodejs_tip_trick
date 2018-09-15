a53347b0e7b94812ae5e6e7fbd80479447d34699
Sonny Nguyen <phplaw@gmail.com>
2018-09-16 04:07:28 +0700

    Update add_html_element_while_page_is_loading.js

--- add_html_element_while_page_is_loading.js
+++ add_html_element_while_page_is_loading.js
@@ -1,1 +1,37 @@
 
+function make_html_input() {
+  var curScriptElement = document.currentScript;
+    
+    var array = ["Volvo","Saab","Mercades","Audi"];
+
+//Create and append select list
+var selectList = document.createElement("select");
+selectList.id = "mySelect";
+//myDiv.appendChild(selectList);
+
+//Create and append the options
+for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    option.value = array[i];
+    option.text = array[i];
+    selectList.appendChild(option);
+}
+    curScriptElement.parentNode.appendChild(selectList);
+  
+  
+  // grab html for using in document.write
+  
+  
+  var el = document.createElement("div");
+      // need clone It and Its childrent
+      el.appendChild(selectList.cloneNode(true));
+      
+      document.write(el.innerHTML);
+  // free memory
+      //el = null;
+      delete el;
+  // end
+  
+    }
+// run this in your html
+// <script>make_html_input()</script>
