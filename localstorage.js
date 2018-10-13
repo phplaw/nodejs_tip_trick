bd6d125963668f2922287369771355b7c8e489e6
Sonny Nguyen <phplaw@gmail.com>
2018-10-13 14:04:55 +0700

    Update localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -7,3 +7,19 @@ localStorage.setItem('testObject', JSON.stringify(testObject));
 var retrievedObject = localStorage.getItem('testObject');
 
 console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+var animal = {
+ name: 'Karl',
+ type: 'cat',
+ color: 'black',
+ age: 7
+};
+
+//convert JSON animal into a string
+var dehydratedAnimal = JSON.stringify(animal);
+
+//save it with local storage
+window.localStorage.setItem('animal', dehydratedAnimal);
+
+//get 'animal' and rehydrate it  (convert it back JSON)
+var rehydratedAnimal = JSON.parse(window.localStorage.getItem('animal'));
