e62282c257634de6bc18defcab81c73206427dc2
Sonny Nguyen <phplaw@gmail.com>
2019-08-03 15:49:12 +0700

    Update date_time.js

--- date_time.js
+++ date_time.js
@@ -1,2 +1,7 @@
 Math.floor(new Date().getTime()/1000.0); // to unix
 var date = new Date(unix_timestamp*1000); // to js date
+//  bluebirdjs and mysql
+const connection = mysql.createConnection({.....});
+global.db  = Bluebird.promisifyAll(connection);
+db.queryAsync("SELECT * FROM users").then(function(rows){   
+console.log(rows);});
